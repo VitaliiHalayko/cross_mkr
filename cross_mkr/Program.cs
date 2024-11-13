809af59fc195de2afaeede8c310d87e99d638b5c
HalaikoVitalii <galaykovitalii@knu.ua>
2024-11-13 09:40:10 +0200

    project

--- cross_mkr/Program.cs
+++ cross_mkr/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace cross_mkr{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n, k;
+			try 
+			{
+				// Check if the input file exists in the project folder
+				(n, k) = IO.readDataFromFile();
+
+				int[] permutation = Permutation.getPermutation(n, k);
+
+				IO.writePermutationToFile(permutation);
+			} 
+			catch (Exception e) 
+			{
+				// Catch any exceptions (e.g., file read or parse errors) and display the error message
+				IO.writeErrorToFile(e.Message);
+			}
+		}
+	}
+}
